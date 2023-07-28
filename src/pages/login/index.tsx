9102609d6e7ba5509a52a8afdd369512b9ec3386
isaac545454 <isaac.newton1221@gmail.com>
2023-07-28 18:19:24 -0300

    feat: create module login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./style";
+import imageStore from "../../../public/Camada_1.png";
+
+export default function Login() {
+  return (
+    <S.Container>
+      <S.BackgroundImage src={imageStore} />
+    </S.Container>
+  );
+}
---
+++ src/pages/login/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const BackgroundImage = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+`;
