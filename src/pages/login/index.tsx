c6bcbb43e11e4fc9d92c0ec514b9a14a5e6e2b6f
isaac545454 <isaac.newton1221@gmail.com>
2023-07-29 14:51:43 -0300

    feat: create login component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,16 @@
 import * as S from "./style";
 import imageStore from "../../../public/Camada_1.png";
+import imageLogo from "../../../public/LOGO.svg";
 
 export default function Login() {
   return (
     <S.Container>
       <S.BackgroundImage src={imageStore} />
+      <S.ContainerLogin>
+        <S.LimitedContainer>
+          <S.LogoImage src={imageLogo} />
+        </S.LimitedContainer>
+      </S.ContainerLogin>
     </S.Container>
   );
 }
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+`;
 
 export const BackgroundImage = styled.img`
   position: absolute;
@@ -9,4 +13,28 @@ export const BackgroundImage = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: -1;
+`;
+
+export const LogoImage = styled.img``;
+
+export const ContainerLogin = styled.div`
+  background-color: #d9d9d9;
+  width: 100%;
+  max-width: 646px;
+  right: 0;
+  top: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 22px;
+  z-index: 2;
+`;
+
+export const LimitedContainer = styled.div`
+  width: 100%;
+  max-width: 498px;
+  display: flex;
+  justify-content: center;
 `;
