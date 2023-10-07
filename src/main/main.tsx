c8eb7f93f8bcc60deb600ef73cc2e6ecff8b2a81
isaac545454 <isaac.newton1221@gmail.com>
2023-10-07 10:04:18 -0300

    chore add zustand

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Login } from '../pages/login/index.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Login } from '../presentation/pages/login'
 
 const queryClient = new QueryClient()
 
