f12fcd2ed4b981fad64dfbfd1bf1347d69476542
isaac545454 <isaac.newton1221@gmail.com>
2023-10-07 08:55:09 -0300

    refactor: infra

--- src/infra/create-http.ts
+++ src/infra/create-http.ts
@@ -1,10 +1,10 @@
-import { AxiosInstance } from "axios";
-import { HttpService } from "./http-service";
-import { apiClient } from "./client-config";
+import { AxiosInstance } from 'axios'
+import { HttpService } from './http-service'
+import { apiClient } from './client-config'
 
 /**
  * Cria uma instância de HttpService configurada para um tipo específico de resposta.
  * @template IResponse - O tipo da resposta esperada.
  * @returns {{ http: HttpService }} Um objeto contendo a instância de HttpService.
  */
-export const createHttp = <IResponse>() => ({ http: new HttpService<IResponse, AxiosInstance>(apiClient)})
+export const createHttp = <IResponse>() => new HttpService<IResponse, AxiosInstance>(apiClient)
