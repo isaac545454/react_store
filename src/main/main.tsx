42c27f5389b5a12d81d1750fa8b26a6cd52a09da
isaac545454 <isaac.newton1221@gmail.com>
2023-10-08 09:19:04 -0300

    chore: react-toastify config

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './style/index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { MakeLogin } from './Factories/pages/login'
+import { ToastContainer } from 'react-toastify'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
+			<ToastContainer />
 			<MakeLogin />
 		</QueryClientProvider>
 	</React.StrictMode>,
